18f45acf3a3e74c824acd98288fd410603ab7dd1
Arsa Fauzi <arsafauzi@gmail.com>
2026-09-26 10:31:05 +0900

    feat(eslint): adding array-callback-return error lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,7 @@ export default tseslint.config(
   {
     rules: {
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'array-callback-return': 'error',
     },
   },
 );
